9e3ca778bade2377f567d729f1c8aa9317ae7d74
Chitwan Kaudan <chitwank@berkeley.edu>
2017-05-30 15:26:59 -0700

    Completed Lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -80,8 +80,14 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        IntList ptr = A;
+        if (ptr.rest == null) {
+            ptr.rest = B;
+        }
+        else {
+            dcatenate (ptr.rest, B);
+        }
+        return A;
     }
 
     /**
@@ -89,8 +95,12 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-        //TODO:  fill in method
-        return null;
+        if (A == null){
+            return B;
+        }
+        else {
+            return new IntList(A.first, catenate(A.rest, B));
+        }
     }
 
 
